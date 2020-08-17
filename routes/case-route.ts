1332ee134ee6007f65a0f13963d9e4b8a26bc475
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-08-17 13:57:36 +0800

    case management finished

--- routes/case-route.ts
+++ routes/case-route.ts
@@ -1,17 +1,31 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import Case from '../src/case-service';
+import Koa from "koa";
+import Router from "koa-router";
+import Case from "../src/case-service";
 
 const router = new Router();
 
-router.prefix('/case')
+router.prefix("/case");
 
-router.get('/', async (ctx: Koa.ParameterizedContext, next: Koa.Next) => {
-  let case_list= Case.getlist()
-  console.log(case_list)
-  await ctx.render('case-management', {case_list:case_list})
-})
-router.get('/create', async (ctx: Koa.ParameterizedContext, next: Koa.Next) => {
-  await ctx.render('create-case')
-})
+router.get("/", async (ctx: Koa.ParameterizedContext, next: Koa.Next) => {
+  let privateFor = ctx.query.privateFor;
+  let searchparam = ctx.query.search;
+
+  if (!Case.checkList(privateFor)) {
+    privateFor = "";
+  }
+  if (!searchparam) {
+    searchparam = "";
+  }
+
+  let case_list = Case.getList(privateFor, searchparam);
+  await ctx.render("case-management", {
+    case_list: case_list,
+    privateFor: privateFor,
+    orgList: Case.orgList.slice(1, Case.orgList.length),
+    searchparam: searchparam,
+  });
+});
+router.get("/create", async (ctx: Koa.ParameterizedContext, next: Koa.Next) => {
+  await ctx.render("create-case");
+});
 export default router;
