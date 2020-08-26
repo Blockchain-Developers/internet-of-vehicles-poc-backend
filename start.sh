04be383de0315ebb8b495e03e7e734f1e0d8c5ef
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-08-26 11:22:39 +0800

    change port from expose to publish

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -td --network iov-poc-ingress --rm -v `pwd`:/app -w /app node:lts-alpine3.12 --expose 80:3000/tcp node ./dist/bin/server.js
+docker run -td --network iov-poc-ingress --rm -v `pwd`:/app -w /app node:lts-alpine3.12 --publish 80:3000/tcp node ./dist/bin/server.js
