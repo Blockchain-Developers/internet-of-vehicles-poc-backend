fa7934734946416f7a38fb37ba81fdb79173afb2
RuiSiang <49300088+RuiSiang@users.noreply.github.com>
2020-08-26 10:57:20 +0800

    added port forwarding to start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -td --network iov-poc-ingress --rm -v `pwd`:/app -w /app node:lts-alpine3.12 node ./dist/bin/server.js
+docker run -td --network iov-poc-ingress --rm -v `pwd`:/app -w /app node:lts-alpine3.12 -p 127.0.0.1:80:3000/tcp node ./dist/bin/server.js
